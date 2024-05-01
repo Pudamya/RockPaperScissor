30b08a9343346b22b83d6800cab5c876da73346e
Pudamya <150059438+Pudamya@users.noreply.github.com>
2024-05-02 00:33:58 +0530

    Update Main.py

--- Main.py
+++ Main.py
@@ -1,19 +1,27 @@
+#Importing modules
 import random
 import sys
 import RockPaperScissor
 import time
 
+#Variable Initialization
 score=10
+name=""
+
+#Display the title of the game
 welcome="Welcome to Rock Paper Scissor Game"
 print(welcome.center(44,"*"))
 print()
 
+#Taking the name of the user as an input
 name=str(input("Please enter your name : "))
 print()
+
+#Printing a welcome for the user.
 print("Hi",name)
 print()
 
-
+#Calling the functions in the RockPaperScissor module
 RockPaperScissor.intro()
 RockPaperScissor.begin()
 rand,Uchoice=RockPaperScissor.begin()
